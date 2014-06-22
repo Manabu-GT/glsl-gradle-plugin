8780c5e69ab9d52977cda678158b9ffee188398b
Manabu-GT <manabu1984+github@gmail.com>
2014-06-22 19:39:34 +0900

    updated test codes

--- src/test/groovy/com/ms/square/plugins/gradle/glsl/GlslPluginTest.groovy
+++ src/test/groovy/com/ms/square/plugins/gradle/glsl/GlslPluginTest.groovy
@@ -1,6 +1,7 @@
 package com.ms.square.plugins.gradle.glsl
 
 import org.gradle.api.Project
+import org.gradle.api.ProjectConfigurationException
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
 
@@ -16,12 +17,19 @@ class GlslPluginTest {
     }
 
     @Test
+    public void pluginWorksWithAndroidLibraryPlugin() {
+        Project project = ProjectBuilder.builder().build()
+        project.apply plugin: 'android-library'
+        project.apply plugin: 'glsl'
+    }
+
+    @Test
     public void pluginFailsWithoutAndroidPlugin() {
         Project project = ProjectBuilder.builder().build()
         try {
             project.apply plugin: 'glsl'
-        } catch(IllegalStateException e) {
-            assertTrue(e.getMessage().equals("The 'android' plugin is required."));
+        } catch(ProjectConfigurationException e) {
+            assertTrue(e.getMessage().equals("The android or android-library plugin must be applied to the project"));
         }
     }
 }
